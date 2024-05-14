a130ef68cf7383e0a9608a20f722d914509fec3d
Valentin C <vcouturier0@gmail.com>
2024-05-14 23:18:55 +0200

    start game with perfect configuration

--- src/app/game.spec.ts
+++ src/app/game.spec.ts
@@ -1,7 +1,38 @@
 import { Game } from './game';
+import { Player } from './player';
 
 describe('Game', () => {
   it('should create an instance', () => {
     expect(new Game()).toBeTruthy();
+  });
+
+  describe('Start method', () => {
+    [
+      {
+        nbPlayers: 2,
+        nbCards: 60,
+        expectedRounds: 30
+      }
+    ].forEach(testCase => {
+      it(`should create ${testCase.expectedRounds} when game has ${testCase.nbCards} cards and ${testCase.nbPlayers} players`, () => {
+        // Arrange
+        let game: Game = new Game();
+
+        for(let i = 0; i < testCase.nbPlayers; ++i){
+          let player: Player = new Player();
+          player.nickname = i.toString();
+          game.players.push(player);
+        }
+
+        game.nbCards = testCase.nbCards;
+
+        // Act
+        game.start();
+
+        // Assert
+        expect(game.rounds).toHaveSize(testCase.expectedRounds);
+      });
+
+    })
   });
 });
--- src/app/game.ts
+++ src/app/game.ts
@@ -1,15 +1,27 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Player } from './player';
+import { Round } from './round';
 
 export class Game {
     id: string;
     status: GameStatus;
     players: Player[];
-
+    nbCards: number;
+    rounds: Round[];
+    
     constructor(){
         this.id = uuidv4();
+        this.nbCards = 60;
         this.status = GameStatus.PlayerSelection;
         this.players = [];
+        this.rounds = [];
+    }
+
+    start() {
+        const nbRound = this.nbCards / this.players.length;
+        for(let i = 0; i < nbRound; ++i){
+            this.rounds.push(new Round());
+        }
     }
 }
 
